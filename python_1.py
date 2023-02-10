131211aebff5d2be281a0b6704a387c0a8beeb71
archit-1203 <architofficial630@gmail.com>
2023-02-10 12:00:43 +0530

    2

--- python_1.py
+++ python_1.py
@@ -2,5 +2,21 @@
 # a=20
 # print(type(a))
 
-b = 51//4
-print(b)
+# b = 51//4
+# print(b)
+
+# l = [1,2,3,4,5,6]
+# print(l)
+# l=[1,2,3,'key',7.9]
+# print(l)
+# print(l[-1])
+
+# tuple = (1,2,3,'key')
+# print(tuple,type(tuple))
+
+Dict = {1:'abcd',2:'bcd',3:'ar'}
+print(Dict,type(Dict))
+Dict = {'name':'abcd',1:[1,2,3,4]}
+print(Dict,type(Dict))
+Dict=dict({1:'dsa',2:'pyton'})
+print(Dict,type(Dict))
