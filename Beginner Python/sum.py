863821be703590724082be212d3c487d4e4f41ef
Archit R Srivastava <99673217+archit1203@users.noreply.github.com>
2026-02-16 02:36:52 +0530

    Create sum.py

---
+++ Beginner Python/sum.py
@@ -0,0 +1,6 @@
+#Write a Program to input 2 numbers & print their sum.
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+sum_result = num1 + num2
+print("The sum of", num1, "and", num2, "is:", sum_result)
