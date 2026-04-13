cebf2ef0d488cf5138666d1bbc2eeaeba2997df0
Archit R Srivastava <99673217+archit1203@users.noreply.github.com>
2026-04-13 23:25:22 +0000

    C2

--- Basic Python Questions/dict1.py
+++ Basic Python Questions/dict1.py
@@ -3,6 +3,8 @@ WC={'India':[1983,2011],'AUS':[1987,1999,2003,2007,2015,2023]}
 print(WC)
 WC['India']=[83,11,27]
 print(WC)
+WC['ENG']=[2019]
+print(WC)
 #Access items of Dict
 #print(WC['India'])
 #print(WC.get("AUS"))
