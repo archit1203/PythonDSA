e2334526ffab76d39320b03b153d073d55b77845
Archit R Srivastava <99673217+archit1203@users.noreply.github.com>
2026-04-13 23:18:11 +0000

    dict 1

---
+++ Basic Python Questions/dict1.py
@@ -0,0 +1,9 @@
+#create a dictionary
+WC={'India':[1983,2011],'AUS':[1987,1999,2003,2007,2015,2023]}
+print(WC)
+WC['India']=[83,11,27]
+print(WC)
+#Access items of Dict
+#print(WC['India'])
+#print(WC.get("AUS"))
+#print(WC.keys())
