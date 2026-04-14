be2df1c47dbcbb709ca5188a00838c3d885b471b
Archit R Srivastava <99673217+archit1203@users.noreply.github.com>
2026-04-14 21:43:43 +0000

    auto-commit

---
+++ LeetCode/1-LC1.py
@@ -0,0 +1,25 @@
+'''
+Given an array of integers nums and an integer target, 
+return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+'''
+
+"""
+Brute Force => O(n^2)
+n=[1]
+key=10
+for i in range(len(n)):
+    temp=key-n[i]
+    for j in range(i+1,len(n)):
+        if temp==n[j]:
+            print(i,j)
+            break
+"""
+
+"""
+Better => Sorting & Binary Seach nlogn
+
+Optimal => 
+"""
