8ffe99a188d8c8d10deec883df63174f3db165e3
Archit R Srivastava <99673217+archit1203@users.noreply.github.com>
2026-04-14 20:36:25 +0000

    oop1

---
+++ Basic Python Questions/Array/array1.py
@@ -0,0 +1,17 @@
+import ctypes
+
+class myList:
+    def __init__(self):
+        self.size=1
+        self.n=0
+        #create a c type array with size=self.size
+        self.A=self.__makeArray(self.size)
+
+    def __makeArray(self,capacity):
+        #creates a ctype static refrential array with size capacity
+        return (capacity*ctypes.py_object)()
+    
+
+L=myList()
+print(L,type(L))
+    
