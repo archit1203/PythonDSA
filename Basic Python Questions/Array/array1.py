e8fc8b5ac209e11904248e960ced23927ec24d73
Archit R Srivastava <99673217+archit1203@users.noreply.github.com>
2026-04-14 20:40:04 +0000

    array2

--- Basic Python Questions/Array/array1.py
+++ Basic Python Questions/Array/array1.py
@@ -7,15 +7,24 @@ class myList:
         #create a c type array with size=self.size
         self.A=self.__makeArray(self.size)
     
+    #LENGTH OF LIST
     def __len__(self):
         return self.n
 
+    #CREATE LIST
     def __makeArray(self,capacity):
         #creates a ctype static refrential array with size capacity
         return (capacity*ctypes.py_object)()
     
+    #APPEND LIST
+
+    
 
 L=myList()
-print(L,type(L))
-print(len(L))
+#print(L,type(L))
+#print(len(L))
+
+L2=[10,20,30]
+L2.append("Hi")
+print(L2)
     
