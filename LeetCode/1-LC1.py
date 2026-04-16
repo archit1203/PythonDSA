cfa219056a19d1f51198cfc4f8c3314a1d2d6e78
Archit R Srivastava <99673217+archit1203@users.noreply.github.com>
2026-04-16 00:35:17 +0000

    lc1

--- LeetCode/1-LC1.py
+++ LeetCode/1-LC1.py
@@ -23,3 +23,11 @@ Better => Sorting & Binary Seach nlogn
 
 Optimal => 
 """
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d=dict()
+        for i in range(len(nums)):
+            d[nums[i]]=i
+        for i in range(len(nums)):
+            need=target-nums[i]
+            if (need in d.keys()) and d[need]!=i:
+                return [i,d[need]]
