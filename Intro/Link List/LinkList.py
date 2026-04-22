e753272185bccd0b33d470dcf1cc920c127ff414
Archit R Srivastava <99673217+archit1203@users.noreply.github.com>
2026-04-22 22:15:08 +0000

    link2

--- Intro/Link List/LinkList.py
+++ Intro/Link List/LinkList.py
@@ -4,16 +4,24 @@ class Node:
         self.next=None
     
 def printList(head):
+    #Traversal: TC = theta(n) ; SC = O(1)
     temp=head
     while temp!=None:
         print(temp.key,end=" -> ")
         temp=temp.next
     print("Null")
     
+def insertBegin(head,k):
+    temp=Node(k)
+    temp.next=head
+    return temp
+
 
 head=Node(10)
 head.next=Node(20)
 head.next.next=Node(30)
+
+head=insertBegin(head,5)
 
 printList(head)
 
