8b5058aa75b4d3b29b68a17d3d68be5544e4f54b
Archit R Srivastava <99673217+archit1203@users.noreply.github.com>
2026-04-26 03:01:18 +0000

    lc21-2

--- LeetCode/7-LC21.py
+++ LeetCode/7-LC21.py
@@ -1,10 +1,12 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+#    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    def mergeTwoLists(self, list1, list2):
         head=ListNode()
         if list1==None:
             if list2==None:
