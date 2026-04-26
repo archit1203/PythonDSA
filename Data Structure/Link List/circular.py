0388ff4ed4392053ae9279c955d395666343f297
Archit R Srivastava <99673217+archit1203@users.noreply.github.com>
2026-04-26 00:50:24 +0000

    cll

---
+++ Data Structure/Link List/circular.py
@@ -0,0 +1,29 @@
+#Circular Linked List
+
+
+
+class Node:
+    def __init__(self,key):
+        self.key=key
+        self.next=None
+
+def printList(head):
+    #Traversal: TC = theta(n) ; SC = O(1)
+    temp=head
+    while temp!=None:
+        print(temp.key,end=" -> ")
+        temp=temp.next
+    print("Null")
+
+
+
+head=Node(10)
+t1=Node(20)
+t2=Node(30)
+
+head.next=t1
+t1.next=t2
+t2.next=head
+
+print("CLL: ",end="")
+printList(head)
