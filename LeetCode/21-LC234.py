d2fd2e260a9b776013d8b8a7934ce11fbb8df0d8
Archit R Srivastava <99673217+archit1203@users.noreply.github.com>
2026-05-10 18:41:21 +0000

    234

--- LeetCode/21-LC234.py
+++ LeetCode/21-LC234.py
@@ -21,4 +21,42 @@ class Solution:
         
         return True
 
+
+"""
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if head is None or head.next is None:
+            return True
+               
+        temp=head
+        count=0
+        while temp:
+            count+=1
+            temp=temp.next
+        
+        stx=[]
+        temp=head
+
+        c=count//2
+
+
+        while c:
+            stx.append(temp.val)
+            temp=temp.next
+            c-=1
+        
+        if count%2!=0:
+            temp=temp.next
+        
+        i=1
+        while temp:
+            t1=stx[-i]
+            if temp.val!=t1:
+                return False
+
+            temp=temp.next
+            i+=1
+        
+        return True
+"""
         
