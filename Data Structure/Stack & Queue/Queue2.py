d51e6fb97b6006f80453de009dd9992edb68cc6f
Archit R Srivastava <99673217+archit1203@users.noreply.github.com>
2026-05-10 17:45:09 +0000

    q21

---
+++ Data Structure/Stack & Queue/Queue2.py
@@ -0,0 +1,17 @@
+#Method 2
+from collections import deque
+
+q=deque()
+q.append(10)
+q.append(20)
+q.append(30)
+
+print(q)
+print(q.popleft())
+
+q.append(40)
+print(q.popleft())
+
+print(len(q))
+print(q[0])
+print(q[-1])
