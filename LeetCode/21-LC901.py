9f28f15e03aa2f734246d6dcb64b200c0753f670
Archit R Srivastava <99673217+archit1203@users.noreply.github.com>
2026-05-26 02:48:57 +0000

    901-2

--- LeetCode/21-LC901.py
+++ LeetCode/21-LC901.py
@@ -4,28 +4,14 @@ class StockSpanner:
         self.stx=[]
                 
     def next(self, price: int) -> int:
-        self.stx.append(price)
-        
-        if len(self.stx)==1:
-            return 1
-        i=len(self.stx)-1
-
         span=1
-        if self.stx[i]<self.stx[i-1]:
-            return 1
-        else:
-            for j in range(i-1,-1,-1):
-                if self.stx[i]>=self.stx[j]:
-                    span+=1
-                else:
-                    return span
-        
-        return span
-            
+        while self.stx and self.stx[-1][0]<=price:
+            span+=self.stx.pop()[1]
+        self.stx.append([price,span])
+        return span            
 
 
 # Your StockSpanner object will be instantiated and called as such:
 # obj = StockSpanner()
 # param_1 = obj.next(price)
-
 """
